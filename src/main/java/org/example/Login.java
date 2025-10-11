d3dd620e87b617669d69efbc51bd4e8f7f74910d
lhhx10 <lhhx10@gmail.com>
2025-10-11 18:38:13 +0900

    Complete login feature

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -3,5 +3,6 @@ package org.example;
 public class Login {
     public static void main(String[] args) {
         System.out.println("Login feature start");
+        System.out.println("Login feature finish");
     }
 }
