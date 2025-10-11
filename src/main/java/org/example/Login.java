86429717605cd9a0849c2eb17070eb3b62e6bd67
lhhx10 <lhhx10@gmail.com>
2025-10-11 23:25:00 +0900

    Edit login feature(1~8)

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -4,5 +4,13 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("Login feature start");
         System.out.println("Login feature finish");
+        System.out.println("Login feature update 1");
+        System.out.println("Login feature update 2");
+        System.out.println("Login feature update 3");
+        System.out.println("Login feature update 4");
+        System.out.println("Login feature update 5");
+        System.out.println("Login feature update 6");
+        System.out.println("Login feature update 7");
+        System.out.println("Login feature update 8");
     }
 }
