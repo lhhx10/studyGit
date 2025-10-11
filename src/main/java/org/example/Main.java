a3dc60bb3497373ac46480076cf3e41e933f4069
lhhx10 <lhhx10@gmail.com>
2025-10-11 23:58:24 +0900

    Edit Main 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
-        System.out.println("Let's study git!");
+        System.out.println("Let's study git!!!");
     }
 }
